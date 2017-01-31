9b0d229bc8a8a7fc64a37a53d680d4d237c85a5e
rhiemer <rodrigohiemer@hotmail.com>
2017-01-31 04:33:59 -0200

    Testes de integração das mudanças nas entidades

--- beerpoints-test-integration/src/test/java/br/com/rhiemer/beerpoints/test/integration/util/BeerPointstWSUtilsArquillian.java
+++ beerpoints-test-integration/src/test/java/br/com/rhiemer/beerpoints/test/integration/util/BeerPointstWSUtilsArquillian.java
@@ -2,8 +2,10 @@ package br.com.rhiemer.beerpoints.test.integration.util;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Collection;
+import java.util.List;
 
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ArchivePaths;
@@ -45,12 +47,25 @@ public class BeerPointstWSUtilsArquillian {
 		File[] libs = Maven.configureResolver().workOffline().withMavenCentralRepo(true).withClassPathResolution(true)
 				.loadPomFromFile("pom.xml").resolve(deps).withTransitivity().asFile();
 
+		List<File> filesList = new ArrayList<>(Arrays.asList(libs));
+		File fileDomain = null;
+		for (File file : filesList) {
+			if (file.getName().indexOf("beerpoints-domain-") > -1) {
+				filesList.remove(file);
+				fileDomain = file;
+				break;
+			}
+		}
+
+		libs = filesList.toArray(new File[] {});
+
 		// gera o projeto ejb
 		JavaArchive jar = ShrinkWrap.create(JavaArchive.class, "beerpoints-service.jar")
 				// gera um beans.xml limpo
 				.addAsManifestResource(EmptyAsset.INSTANCE, ArchivePaths.create("beans.xml"))
 				// injeta persistence service
-				.addAsResource("META-INF/persistence.xml").addAsResource("META-INF/orm.xml")
+				.addAsResource("META-INF/persistence-test.xml", "META-INF/persistence.xml")
+				.addAsResource("META-INF/orm.xml")
 				// injeta arquivo de resorces do projeto
 				.addAsResource("environment.properties")
 				// adiciona todos os pacotes necessários ao projeto
@@ -72,6 +87,9 @@ public class BeerPointstWSUtilsArquillian {
 				// contexto web apontando para test afim de não conflitar com
 				// o contexto da aplicação
 				.setApplicationXML("META-INF/application.xml").addAsModule(jar).addAsModule(war);
+		if (fileDomain != null) {
+			ear.addAsLibrary(fileDomain, "beerpoints-domain.jar");
+		}
 
 		// exporta o deploy gerado para verificações
 		// exportDeploy(ear);
